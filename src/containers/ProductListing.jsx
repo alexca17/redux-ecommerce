9029d95bdbeb722404d93417bf1b4785f1fe0c12
Alejandro <alex_carrasco17@hotmail.com>
2022-02-14 14:20:33 -0700

    Se corrige build

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -5,7 +5,6 @@ import { setProducts } from "../redux/actions/productActions";
 import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
-  const products = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const fetchProducts = async () => {
